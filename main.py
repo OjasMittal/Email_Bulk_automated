f213219b2b55e0d12e085666ac5c5034f39e97a9
Ojas Mittal <93815296+OjasMittal@users.noreply.github.com>
2022-03-11 11:22:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def send_email():
     news_feed = NewsFeed(interest=row['interest'],
                          from_date=yesterday,
                          to_date=today)
-    email = yagmail.SMTP(user="automail.ojas.python@gmail.com", password="ojaspython123")
+    email = yagmail.SMTP(user="automail.ojas.python@gmail.com", password="ENTER PASSWORD HERE")
     email.send(to=row['email'],
                subject=f"Your {row['interest']} news for today!",
                contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n{news_feed.get()}\nArdit")
 
 
 while True:
-    if datetime.datetime.now().hour == 22 and datetime.datetime.now().minute == 38:
+    if datetime.datetime.now().hour == 10 and datetime.datetime.now().minute == 15:
         df = pandas.read_excel('people.xlsx')
 
         for index, row in df.iterrows():
